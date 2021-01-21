b30c91162619d492e7542a278a6ae8424efb3a70
mzpolini <matthew.polini@gmail.com>
2021-01-21 15:16:29 -0500

    feat: gets contentful source up and running

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,22 @@
 module.exports = {
+  siteMetadata: {
+    title: `the Gatsby Sandbox`,
+  },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: 'kb9wgir596y6',
+        accessToken: 'muJu7wiDT7BapDXnltXtYalMWSU84hQbHzH7Sx0fHSY',
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `./src/lib/data/`,
       },
     },
+    `gatsby-plugin-react-helmet`,
     `gatsby-transformer-json`,
   ],
 }
